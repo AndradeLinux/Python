eb893a24feb406cfe5c93c1646326f0275ac020c
Gabriel Moraes de Andrade <gbmoraescm@gmail.com>
2021-09-02 22:13:48 -0300

    v1.0
    Primeiros códigos.

---
+++ desafios/desafio002.py
@@ -0,0 +1,6 @@
+print('========== DESAFIO 002 ===========')
+dia=input('Dia = ')
+mes=input('Mês = ')
+ano=int(input('Ano = '))
+idade=2021-ano
+print('Você nasceu no dia', dia, 'de', mes, 'de', ano, 'e tem', idade, 'anos. Certo? ')
