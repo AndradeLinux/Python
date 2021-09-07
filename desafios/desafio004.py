fef50a179b587b2e9a2031063ddb51210bba65b5
Gabriel Moraes de Andrade <gbmoraescm@gmail.com>
2021-09-06 23:53:49 -0300

    06 de setembro de 2021
    Adicionei a pasta /project com dois arquivos de aula dentro, aulao6a.py e aula06b.py
    Alterei o arquivo desafio003.py  e adicionei os arquivos desafio004.py e desafio005.py à pasta /desafios.
    Adicionei o arquivo ex002.py à pasta /exercicios.

---
+++ desafios/desafio004.py
@@ -0,0 +1,5 @@
+print('========== DESAFIO 04 ===========')
+num1=int(input('Digite um número: '))
+num2=int(input('Digite outro número: '))
+soma=num1+num2
+print('A soma entre {0} e {1} vale {2}.' .format(num1, num2, soma))
--- project/aula06a.py
+++ project/aula06a.py
@@ -1,4 +1,3 @@
-print('========== DESAFIO 003 ==========')
 num1=int(input('Primeiro número: '))
 num2=int(input('Segundo número: '))
 soma=num1+num2
